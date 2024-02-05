a56522d242519a12208b5c7d5dc53443a9f38d0b
Shrey-Banugaria <shrey@ninjatechnolabs.com>
2024-02-05 19:04:34 +0530

    Added Test Routes

--- app/common/middleware.js
+++ app/common/middleware.js
@@ -10,7 +10,7 @@ const authenticateUser = async (req, res, next) => {
       const decoded = jwt.verify(token, JWT_SECRET_KEY);
       const user = await User.findById(decoded.userId);
       if (!user) return res.status(401).json({ error: 'Unauthorized - Invalid user' });
-  
+      if (!user.isActive) return res.status(401).json({ error: 'User account Deleted' });
       req.user = user
       next();
     } catch (error) {
