15aa97adbc76426632049a5ca30e2aa251953c41
Shreybanugariya <shreybanugaria10@gmail.com>
2024-02-09 00:18:24 +0530

    updated weebhook

--- app/common/controllers.js
+++ app/common/controllers.js
@@ -1,24 +1,28 @@
 const controllers = {}
-const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY
+const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID
 const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET
+const SECRET_KEY = process.env.SECRET_KEY || 'Agan_pankh'
 const amount = process.env.PREMINUM_AMOUNT || 499
+const crypto = require('crypto')
 
-controllers.createPayment = async (req, res) => {
+const Razorpay = require('razorpay')
+
+controllers.createOrder = async (req, res) => {
     const razorpay = new Razorpay({
         key_id: RAZORPAY_KEY_ID,
         key_secret: RAZORPAY_KEY_SECRET,
     })
 
-    const { _id, email, username, contactNo, googleId } = req.user
+    // const { _id, email, username, contactNo, googleId } = req.user
     const options = {
         amount: amount * 100, // rzp format with paise
         currency: 'INR',
-        receipt: `${googleId}`, //Receipt no that corresponds to this Order,
+        receipt: `123`, //Receipt no that corresponds to this Order,
         payment_capture: true,
         notes: {
          orderType: "Preminum",
-         user: _id,
-         userEmail: email
+         user: 'test',
+         userEmail: 'test'
         }
     }
     try {
@@ -27,6 +31,21 @@ controllers.createPayment = async (req, res) => {
         return res.status(200).json({ message: 'Payment Success', order_id: response.id, currency: response.currency, amount: response.amount })
     } catch (err) {
        res.status(400).send('Not able to create order. Please try again!');
+    }
+}
+
+controllers.paymentCapture = async (req, res) => {
+    const data = crypto.createHmac('sha256', SECRET_KEY)
+    data.update(JSON.stringify(req.body))
+    const digest = data.digest('hex')
+    console.log(req.headers['x-razorpay-signature'])
+    if (digest === req.headers['x-razorpay-signature']) {
+        console.log('request is legit')
+        res.json({
+            status: 'ok'
+        })
+    } else {
+        res.status(400).send('Invalid signature');
     }
 }
 
--- app/common/routes.js
+++ app/common/routes.js
@@ -3,6 +3,7 @@ const controllers = require('./controllers');
 const authMiddleware = require('./middleware')
 
 
-router.post('/create-payment', controllers.createPayment)
+router.post('/create-payment', controllers.createOrder)
+router.post('/order/capture-payment', controllers.paymentCapture)
 
 module.exports = router;
