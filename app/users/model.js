1037e15c04697a80085c1d7a5ed0dae750f1c7d3
Shreybanugariya <shreybanugaria10@gmail.com>
2024-02-11 02:38:01 +0530

    updated user model

--- app/users/model.js
+++ app/users/model.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
   isAdmin: { type: Boolean, default: false },
   hasPreminum: { type: Boolean, default: false },
   authToken: { type: String },
-  currentTestIndex: { type: Number, enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], required: true, unique: true, default: 0 },
+  currentTestIndex: { type: Number, enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], required: true, default: 0 },
   city: { type: String },
   isActive: { type: Boolean, default: true },
 });
