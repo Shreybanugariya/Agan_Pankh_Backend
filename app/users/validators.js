b445dacad9aa3a4551c0e6a0fa9fa633b0d27a25
Shreybanugariya <shreybanugaria10@gmail.com>
2024-02-15 00:14:26 +0530

    Update user validator updated

--- app/users/validators.js
+++ app/users/validators.js
@@ -7,7 +7,6 @@ validators.createUser = (req, res, next) => {
 }
 
 validators.updateUser = (req, res, next) => {
-    if (!req.params.id) return res.reply(message.required_field('id'));
     if (!req.body.contactNo) return res.reply(message.required_field('contactNo'));
     if (!req.body.city) return res.reply(message.required_field('city'));
     next();
