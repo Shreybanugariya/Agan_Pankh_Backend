c1b8e176502be7267f9c3554f7042ea05b6ba206
Shreybanugariya <shreybanugaria10@gmail.com>
2024-02-15 01:25:57 +0530

    updated function

--- app/common/functions.js
+++ app/common/functions.js
@@ -50,7 +50,7 @@ const calculateScore = (questions, answers) => {
 common.submitTestAndCalulateResult = async ({ userId, testId }, submitedAnswers) => {
   const test = await Tests.findById(testId);
   const testResults = await TestResults.findOne({ userId, testId }).lean()
-  await TestResults.updateOne({ _id: testResults._id }, { isCompleted: true, score: 0 });
+  await TestResults.updateOne({ _id: testResults._id }, { isCompleted: true });
   await User.updateOne({ _id: userId }, { currentTestIndex: test.testIndex + 1 })
   if (!test) return 0;
   if (submitedAnswers && submitedAnswers.length) {
