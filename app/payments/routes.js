c9d01a558d22815d6ad3975a67934c900a48b31c
Shreybanugariya <shreybanugaria10@gmail.com>
2024-02-18 16:54:32 +0530

    Updated webhook to post method

--- app/payments/routes.js
+++ app/payments/routes.js
@@ -3,7 +3,7 @@ const controllers = require('./controllers');
 const authMiddleware = require('../common/middleware')
 
 router.post('/create-payment', authMiddleware, controllers.createOrder)
-router.get('/order/capture-payment', controllers.paymentCapture) // Webhook
+router.post('/order/capture-payment', controllers.paymentCapture) // Webhook
 router.post('/create-payment/upi', authMiddleware, controllers.createUPILink)
 
 module.exports = router;
