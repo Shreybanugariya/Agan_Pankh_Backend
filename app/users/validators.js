ef8cf02cd3170f2f0a9fa60be2e4aacf7e4ec0ed
Shreybanugariya <shreybanugaria10@gmail.com>
2024-02-18 17:09:43 +0530

    Added username validation

--- app/users/validators.js
+++ app/users/validators.js
@@ -9,7 +9,7 @@ validators.createUser = (req, res, next) => {
 validators.updateUser = (req, res, next) => {
     if (!req.body.contactNo) return res.reply(message.required_field('contactNo'));
     if (!req.body.city) return res.reply(message.required_field('city'));
-    // if (!req.body.username) return res.reply(message.required_field('username'));   
+    if (!req.body.username) return res.reply(message.required_field('username'));   
     next();
 }
 
