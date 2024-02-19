7aba20c819e49a88198e4eb1e4b638989380574d
Shrey-Banugaria <shrey@ninjatechnolabs.com>
2024-02-19 19:07:04 +0530

    Removed required filed for the questionIndex

--- app/testresults/model.js
+++ app/testresults/model.js
@@ -6,7 +6,7 @@ const testResultSchema = new mongoose.Schema({
   isCompleted: { type: Boolean, default: false }, // Whenever the test is submited or the time completes this field will have value: true
   answers: [
     {
-      questionIndex: { type: Number, required: true, index: true },
+      questionIndex: { type: Number, index: true },
       question: { type: mongoose.Schema.Types.ObjectId, ref: 'Tests.questions' },
       selectedOptionIndex: { type: Number },
     },
