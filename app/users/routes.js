e9f0667372f4a23604d9067af98e33392eabbea5
Shrey-Banugaria <shrey@ninjatechnolabs.com>
2024-03-14 11:39:41 +0530

    updated users routes

--- app/users/routes.js
+++ app/users/routes.js
@@ -9,9 +9,9 @@ router.post('/auth/google-signin', createUser, controllers.googleSingIn)
 router.put('/update/:id?', updateUser, authenticateUser, controllers.updateUser)
 
 // Admin
-router.post('/admin/auth/login', adminAuthMiddleware, controllers.adminLogin)
-router.get('/admin/getUsers', adminAuthMiddleware, controllers.adminGetUsers)
-router.post('/admin/add-user', adminAuthMiddleware, controllers.adminAddUsers)
-router.delete('/admin/delete-user/:id', adminAuthMiddleware, controllers.adminGetUsers)
+router.post('/admin/auth/login', controllers.adminLogin)
+router.get('/admin/getUsers',  controllers.adminGetUsers)
+router.post('/admin/add-user', controllers.adminAddUsers)
+router.delete('/admin/delete-user/:id', controllers.adminGetUsers)
 
 module.exports = router;
